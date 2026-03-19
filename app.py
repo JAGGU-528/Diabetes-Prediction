dc836f274e90052e313c352270b1d064cc3562c9
JAGGU-528 <jagdishbiradar786@gmail.com>
2026-03-19 22:05:15 +0530

    fix os import position

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
 import pandas as pd
+import os
 
 app = Flask(__name__)
 
@@ -30,7 +31,6 @@ def predict():
     result = 'Has Diabetes' if prediction == 1 else 'No Diabetes'
     return jsonify({'prediction': result})
 
-import os
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
